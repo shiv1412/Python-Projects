ed42e9d8a3d5ec1b1ecd4c6dbc1d6970ec77bae9
shivani-sharma <59807961+shiv1412@users.noreply.github.com>
2020-03-12 12:13:29 -0400

    Update hypothesis_models_practice.py

--- hypothesis_models_practice.py
+++ hypothesis_models_practice.py
@@ -160,3 +160,53 @@ from statsmodels.formula.api import ols
 model = ols('value ~ C(Genotype) + C(years) + C(Genotype):C(years)', data=d_melt).fit()
 anova_table = sm.stats.anova_lm(model, typ=2)
 anova_table
+
+
+#chi-squared test
+import numpy as np
+a1 = [6, 4, 5, 10]
+a2 = [8, 5, 3, 3]
+a3 = [5, 4, 8, 4]
+a4 = [4, 11, 7, 13]
+a5 = [5, 8, 7, 6]
+a6 = [7, 3, 5, 9]
+dice = np.array([a1, a2, a3, a4, a5, a6])
+from scipy import stats
+
+stats.chi2_contingency(dice)
+chi2_stat, p_val, dof, ex = stats.chi2_contingency(dice)
+print("===Chi2 Stat===")
+print(chi2_stat)
+print("\n")
+print("===Degrees of Freedom===")
+print(dof)
+print("\n")
+print("===P-Value===")
+print(p_val)
+print("\n")
+print("===Contingency Table===")
+print(ex)
+r1 = np.random.randint(1,7,1000)
+r2 = np.random.randint(1,7,1000)
+r3 = np.random.randint(1,7,1000)
+r4 = np.random.randint(1,7,1000)
+r5 = np.random.randint(1,7,1000)
+
+unique, counts1 = np.unique(r1, return_counts=True)
+unique, counts2 = np.unique(r2, return_counts=True)
+unique, counts3 = np.unique(r3, return_counts=True)
+unique, counts4 = np.unique(r4, return_counts=True)
+unique, counts5 = np.unique(r5, return_counts=True)
+
+dice = np.array([counts1, counts2, counts3, counts4, counts5])
+chi2_stat, p_val, dof, ex = stats.chi2_contingency(dice)
+
+my_rolls_expected = [46.5, 46.5, 46.5, 46.5, 46.5, 46.5]
+my_rolls_actual =  [59, 63, 37, 38, 32, 50]
+st1 = stats.chisquare(my_rolls_actual, my_rolls_expected)
+print(st1)
+opp_rolls_expected = [50.5,50.5,50.5,50.5,50.5,50.5]
+opp_rolls_actual =  [39,39,46,54,53,72]
+st2 = stats.chisquare(opp_rolls_actual, opp_rolls_expected)
+print(st2)
+
