23530795ec19e07f80daa959d5c936084a135c03
shivani-sharma <59807961+shiv1412@users.noreply.github.com>
2020-03-14 23:27:01 -0400

    Update random_forest.py

--- random_forest.py
+++ random_forest.py
@@ -5,14 +5,13 @@ Created on Thu Mar 12 00:17:43 2020
 @author: sharm
 """
 
-#random forest algorithm
 from sklearn import datasets
-import pandas as pd
-iris =datasets.load_iris()
+
+iris = datasets.load_iris()
 print(iris.target_names)
-print(iris.features_names)
-print(iris.data[0:5])
-print(iris.target)
+print(iris.feature_names)
+
+import pandas as pd
 data=pd.DataFrame({
     'sepal length':iris.data[:,0],
     'sepal width':iris.data[:,1],
@@ -21,3 +20,62 @@ data=pd.DataFrame({
     'species':iris.target
 })
 data.head()
+
+from sklearn.model_selection import train_test_split
+
+X=data[['sepal length', 'sepal width', 'petal length', 'petal width']]
+y=data['species'] 
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+from sklearn.ensemble import RandomForestClassifier
+
+clf = RandomForestClassifier(n_estimators=100)
+clf.fit(X_train,y_train)
+y_pred = clf.predict(X_test)
+
+from sklearn import metrics
+print("Accuracy", metrics.accuracy_score(y_test,y_pred))
+print("predicted value ", clf.predict([[3,5,4,2]]))
+
+from sklearn.ensemble import RandomForestClassifier
+
+clf = RandomForestClassifier(n_estimators = 100)
+
+clf.fit(X_train,y_train)
+
+RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini',
+            max_depth=None, max_features='auto', max_leaf_nodes=None,
+            min_impurity_decrease=0.0, min_impurity_split=None,
+            min_samples_leaf=1, min_samples_split=2,
+            min_weight_fraction_leaf=0.0, n_estimators=100, n_jobs=1,
+            oob_score=False, random_state=None, verbose=0,
+            warm_start=False)
+
+import pandas as pd
+feature_imp = pd.Series(clf.feature_importances_,index=iris.feature_names).sort_values(ascending=False)
+print(feature_imp)
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.barplot(x=feature_imp, y=feature_imp.index)
+plt.xlabel('Feature Importance Score')
+plt.ylabel('Features')
+plt.title("Visualizing Important Features")
+plt.legend()
+plt.show()
+from sklearn.ensemble import RandomForestClassifier
+
+#Create a Gaussian Classifier
+clf=RandomForestClassifier(n_estimators=100)
+
+#Train the model using the training sets y_pred=clf.predict(X_test)
+clf.fit(X_train,y_train)
+y_pred=clf.predict(X_test)
+
+#Import scikit-learn metrics module for accuracy calculation
+from sklearn import metrics
+# Model Accuracy, how often is the classifier correct?
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+
